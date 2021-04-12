4a5f98b9dfd0b612e298b52c68d4bded6f308ba0
fer vargas <fvargs33@gmail.com>
2021-04-12 15:56:49 -0500

    frist commit

---
+++ resources/views/graphics/grafica4.blade.php
@@ -0,0 +1,74 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Grafica 4') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div id=container_graphic></div>
+            </div>
+        </div>
+    </div>
+    <script>
+        
+        Highcharts.chart('container_graphic', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Mi tercer gráfica'
+            },
+            xAxis: {
+                categories:{!!$data->pluck("title")!!},
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Total de rentas por película'
+                },
+                stackLabels: {
+                    enabled: true,
+                    style: {
+                        fontWeight: 'bold',
+                        color: ( // theme
+                            Highcharts.defaultOptions.title.style &&
+                            Highcharts.defaultOptions.title.style.color
+                        ) || 'gray'
+                    }
+                }
+            },
+            legend: {
+                align: 'right',
+                x: -30,
+                verticalAlign: 'top',
+                y: 25,
+                floating: true,
+                backgroundColor:
+                    Highcharts.defaultOptions.legend.backgroundColor || 'white',
+                borderColor: '#CCC',
+                borderWidth: 1,
+                shadow: false
+            },
+            tooltip: {
+                headerFormat: '<b>{point.x}</b><br/>',
+                pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+            },
+            plotOptions: {
+                column: {
+                    stacking: 'normal',
+                    dataLabels: {
+                        enabled: true
+                    }
+                }
+            },
+            series: [{
+                name:'Peliculas',
+                data:{!!$data->pluck("quantity")!!},
+            }]
+        });
+
+    </script>
+    </x-app-layout>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/ 
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get("grafica1", [\App\Http\Controllers\GraphicController::class,'showGraphic1'])->name("grafica1");
+Route::get("grafica_top_5", [\App\Http\Controllers\GraphicController::class,'showGraphic2'])->name("grafica_top_5");
+Route::get("grafica_comentarios", [\App\Http\Controllers\GraphicController::class,'showGraphicComments'])->name("grafica_comentarios");
+Route::get("grafica_final", [\App\Http\Controllers\GraphicController::class,'showGraphicFinal'])->name("grafica_final");
+
+
+Route::get("fer",function(){
+	return view("fer.index");
+})->name("route_fer");
+
